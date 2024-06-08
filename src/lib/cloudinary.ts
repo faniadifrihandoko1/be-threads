66a4192f93c9f5de696b7b453d8f13558e4c6201
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2024-06-08 15:42:26 +0700

    fix: error deploy cloudinary

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from "cloudinary";
 export default new (class CloudinaryConfig {
   config() {
     cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.API_KEY,
-      api_secret: process.env.API_SECRET,
+      cloud_name: "dipdenbch",
+      api_key: "954663778361164",
+      api_secret: "sRSooV_rYkEyzvgL8Qip92zpfhM",
     });
   }
 
